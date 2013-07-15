d003f9e69c27578dabe12f5fb120b6c8312a5c83
Timethor <Nick.o.rich@gmail.com>
2013-07-15 02:29:35 -0400

    Original TC InvalidConfigException

---
+++ core/src/main/java/com/timethor/terracontrol/core/exception/InvalidConfigException.java
@@ -0,0 +1,9 @@
+package com.timethor.terracontrol.core.exception;
+
+@SuppressWarnings("serial") // No need to serialize this
+public class InvalidConfigException extends Exception {
+
+    public InvalidConfigException(String string) {
+        super(string);
+    }
+}
